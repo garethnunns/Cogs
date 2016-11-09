6989cff9658a4f246f937e438668ade20175ae8b
Gareth Nunns <garethnunns@gmail.com>
2016-11-09 13:25:39 +0000

    Pointless extras

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 	<head>
 		<title>Cogs Helpdesk</title>
 
+		<meta charset="utf-8">
+
 		<link rel="stylesheet" type="text/css" href="site/cogs.css?version=<?php echo time(); // remove when out of dev ?>">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
 	</head>
--- pages/settings.php
+++ pages/settings.php
@@ -2,4 +2,4 @@
 	require_once dirname(__FILE__).'/../check.php';
 ?>
 
-<p>settingy mcsettingsface</p>
+<p>settingsy mcsettingsface</p>
