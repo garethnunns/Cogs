e922b692f16cc6b3b57b5c4248732bbc1cc6b8c7
Gareth Nunns <garethnunns@gmail.com>
2017-02-19 05:12:58 +0000

    Problems
    I’ve got 99 problems and they’re all shown on the website now
    
    COGS-12 #time 30m With the problems page and functions made, this will
    make the new call page easier to produce
    
    COGS-14 #time 3h Pretty much working, needs a tweak to the CSS, as well
    as the ability to edit everything
    
    COGS-15 #time 15m Works exactly the same as the problems page, so needs
    to have the outputProblems function adjusted and tweaks to the SQL

--- functions.php
+++ functions.php
@@ -7,15 +7,19 @@ Change log
 
 18/2/17 - Gareth Nunns
 Added translate function outline
+Added longAgoEpoch function
+Added storeProblem function
+Updated outputProblem function
+Removed outputResponse function
 
 14/2/17 - Gareth Nunns
 Added changelog
 
 */
 
-	function translate($phrase, $lang) {
+	function translate($phrase) {
 		/*
-		Function to output $phrase in $lang
+		Function to output $phrase in users selected lang
 
 		Psuedocode:
 		function translate(phrase, lang) {
@@ -46,197 +50,306 @@ Added changelog
 		}
 	}
 
+	function longAgoEpoch($date) {
+		if(($diff = time() - $date) <= 30)
+			return 'A few seconds ago';
+		else if($diff <= 4*60)
+			return 'A few minutes ago';
+		else if($diff < 60*60)
+			return floor($diff/60).' minutes ago';
+		else if($diff < 24*60*60)
+			return floor($diff/(60*60)).' hours ago';
+		else if($diff < 365*24*60*60)
+			return floor($diff/(24*60*60)).' days ago';
+	}
+
 	function isValidTimezone($timezone) {
 		@$tz=timezone_open($timezone);
 		return $tz!==FALSE;
 	}
 
+	function storeProblems($probs) {
+		// because the data structure is fairly complex, it's restructured in PHP because it's simpler than adding a lot of SQL
+		// this will be a standard structure so it can be reused on many pages to output various tables
+		// the problems are stored in this $problems array
+		$problems = array();
 
-	// temp functions
-	function outputProblem($problem,$solved) {
-		global $format, $tlogin, $tproblems;
+		foreach ($probs as $row) {
+			if(!array_key_exists($row['idProblem'], $problems)) // this problem hasn't been logged at all yet
+				// so initialise a new empty array and store the generic attributes of the problem
+				$problems[$row['idProblem']] = array(
+					'title' => $row['title'],
+					'type' => array(
+						'id' => $row['idType'],
+						'name' => $row['type']
+					),
+					// initialise an empty array to store all of the events in the problem
+					'events' => array()
+				);
 
+			// messages
+			$found = false; // variable to be used when traversing arrays
+			// where the message will be positioned in the array
+			$pos = count($problems[$row['idProblem']]['events'])-1; 
+
+			foreach ($problems[$row['idProblem']]['events'] as $key => $event) {
+				if(($event['type']=='message') && ($event['id']==$row['idMessage'])) {
+					$found = true; // the message is already stored
+					break; // stop searching for efficiency
+				}
+				if($event['date'] > strtotime($row['messDate'])) 
+					$pos = $key; // so that they're stored in descending date order
+			}
+			if(!$found && !empty($row['idMessage'])) // the message isn't already in the array
+				// add the message in the correct position
+				array_splice($problems[$row['idProblem']]['events'], $pos+1, 0, array(
+					array(
+						'type' => 'message',
+						'date' => strtotime($row['messDate'].' '.date_default_timezone_get()),
+						'id' => $row['idMessage'],
+						'subject' => $row['messSub'],
+						'message' => $row['messMess'],
+						'specialist' => array(
+							'id' => $row['messSpec'],
+							'name' => $row['messName'],
+							'tel' => $row['messTel'],
+							'job' => $row['messJob']
+						)
+					)
+				));
+
+			// assignments
+			// reset vars
+			$found = false;
+			$pos = count($problems[$row['idProblem']]['events'])-1; 
+			
+			foreach ($problems[$row['idProblem']]['events'] as $key => $event) {
+				if(($event['type']=='assign') && ($event['id']==$row['idAssign'])) {
+					$found = true; // the assignment is already stored
+					break; // stop searching for efficiency
+				}
+				if($event['date'] > strtotime($row['assDate'])) 
+					$pos = $key; // so that they're stored in descending date order
+			}
+			if(!$found && !empty($row['idAssign'])) // the assignment isn't already in the array
+				// add the assignment in the correct position
+				array_splice($problems[$row['idProblem']]['events'], $pos+1, 0, array(
+					array(
+						'type' => 'assign',
+						'date' => strtotime($row['assDate'].' '.date_default_timezone_get()),
+						'id' => $row['idAssign'],
+						'by' => array(
+							'id' => $row['assBy'],
+							'name' => $row['assByName']
+						),
+						'to' => array(
+							'id' => $row['assTo'],
+							'name' => $row['assToName']
+						)
+					)
+				));
+
+			// calls
+			// reset vars
+			$found = false;
+			$pos = count($problems[$row['idProblem']]['events'])-1; 
+			
+			foreach ($problems[$row['idProblem']]['events'] as $key => $event) {
+				if(($event['type']=='call') && ($event['id']==$row['idCalls'])) {
+					$found = true; // the call is already stored
+					break; // stop searching for efficiency
+				}
+				if($event['date'] > strtotime($row['callDate'])) 
+					$pos = $key; // so that they're stored in descending date order
+			}
+			if(!$found && !empty($row['idCalls'])) // the call isn't already in the array
+				// add the call in the correct position
+				array_splice($problems[$row['idProblem']]['events'], $pos+1, 0, array(
+					array(
+						'type' => 'call',
+						'date' => strtotime($row['callDate'].' '.date_default_timezone_get()),
+						'id' => $row['idCalls'],
+						'subject' => $row['callSubject'],
+						'message' => $row['callNotes'],
+						'caller' => array(
+							'id' => $row['caller'],
+							'name' => $row['callerName'],
+							'tel' => $row['callerTel'],
+							'job' => $row['callerJob']
+						),
+						'specialist' => array(
+							'id' => $row['op'],
+							'name' => $row['opName'],
+							'tel' => $row['opTel'],
+							'job' => $row['opJob']
+						)
+					)
+				));
+		}
+
+		return $problems;
+	}
+
+	function outputProblem($id,$problem) {
+		// empty arrays to store the names in
 		$customers = array();
 		$ops = array();
-		
-		foreach ($problem['calls'] as $call) {
-			if(!in_array($call['caller'],$customers))
-				array_push($customers, $call['caller']);
-			if(!in_array($call['op'],$ops))
-				array_push($ops, $call['op']);
-		}
-		
-		foreach ($problem['messages'] as $mess)
-			if(!in_array($mess['op'],$ops))
-				array_push($ops, $mess['op']);
-		
-		foreach ($problem['assign'] as $ass)
-			if(!in_array($ass['op'],$ops))
-				array_push($ops, $ass['op']);
 
-		echo "<tr id='prob{$problem['id']}'>
-		<td rowspan='2'>{$problem['id']}</td>
+		// number of calls and messages
+		$calls = 0;
+		$messages = 0;
+		$solved = 0;
+		
+		foreach ($problem['events'] as $event) // find all the people associated with the problem
+			switch ($event['type']) {
+				case 'call':
+					$calls++;
+					if(!in_array($event['caller']['name'],$customers)) // store the customers
+						array_push($customers, $event['caller']['name']);
+					if(!in_array($event['specialist']['name'],$ops)) // store the call operator
+						array_push($ops, $event['specialist']['name']);
+					break;
+				case 'message':
+					$messages++;
+					if(!in_array($event['specialist']['name'],$ops)) // store the message sender
+						array_push($ops, $event['specialist']['name']);
+					break;
+				case 'assign':
+					if(!in_array($event['to']['name'],$ops)) // store the assigned specialist
+						array_push($ops, $event['to']['name']);
+					break;
+				case 'solved':
+					$solved++;
+					break;
+			}
+
+		echo "<tr id='prob$id'>
+		<td rowspan='2'>$id</td>
 		<td rowspan='2'>{$problem['title']}</td>
 
 		<td rowspan='2'>";
 
-		foreach ($customers as $num => $customer)
+		foreach ($customers as $num => $customer) // output the list of customers
 			echo $customer.($num==count($customers)-1 ? '' : '<br>');
 
 		echo "</td>
 		<td rowspan='2'>";
 
-		foreach ($ops as $num => $op)
-			echo $tlogin[$op]['name'].($num==count($ops)-1 ? '' : '<br>');
+		foreach ($ops as $num => $op) // output the list of operators
+			echo $op.($num==count($ops)-1 ? '' : '<br>');
 
 		echo "</td>
 
-		<td rowspan='2' class='numCalls'>
-		<span>".count($problem['calls'])."</span>
-		<br>".(count($problem['calls'])==1 ? 'Call' : 'Calls')."</td>";
+		<td class='numCalls'>
+		<span>".$calls."</span>
+		<br>".translate($calls==1 ? 'Call' : 'Calls')."</td>";
 
-		if($problem['solution']) {
-			echo "<td>Solved</td><td>";
-		
-			echo date($format,strtotime($problem['solution']['date']));
-			if($ago = longAgo($problem['solution']['date']))
+		if($solved) {
+			echo "<td>Solved ($solved)</td><td>";
+
+			foreach ($problem['events'] as $event) // look for the most recent solution date
+				if($event['type']=='solved')
+					$latestSolved = $event['date'];
+
+			echo date($format,$latestSolved);
+			if($ago = longAgoEpoch($latestSolved))
 				echo '<br>('.$ago.')';
 		}
 		else {
-			echo "<td>Most recent</td><td>";
+			if($problem['events'][0]) { // if there has been any activity on the problem
+				echo "<td>Most recent</td><td>";				
 
-			echo date($format,strtotime($problem['calls'][0]['date']));
+				$latest = $problem['events'][0]['date'];
+
+				echo date($format,$latest);
 			
-			if($ago = longAgo($problem['calls'][0]['date']))
-				echo '<br>('.$ago.')';
+				if($ago = longAgoEpoch($latest))
+					echo '<br>('.$ago.')';
+			}
+			else
+				echo "<td colspan='2' rowspan='2'>".translate('No activity on this problem');
 		}
 
 		echo "</td>
 		</tr>
 
 		<tr>
-		<td>Reported</td>
-		<td>";
-		
-		echo date($format,strtotime(end($problem['calls'])['date']));
-		if($ago = longAgo(end($problem['calls'])['date']))
-			echo '<br>('.$ago.')';
 
-		echo "</td>
-		</tr>
+		<td class='numCalls'>
+		<span>".$messages."</span>
+		<br>".translate($messages==1 ? 'Message' : 'Messages')."</td>";
+
+		if($problem['events'][0]) {
+			echo "<td>Earliest</td>
+			<td>";
+			
+			$earliest = $problem['events'][count($problem['events'])-1]['date'];
+
+			echo date($format,$earliest);
+		
+			if($ago = longAgoEpoch($earliest))
+				echo '<br>('.$ago.')';
+
+			echo "</td>";
+		}
+		
+		echo "</tr>
 
 		<tr class='responses'>
 		<td colspan='7'>
 		<h2>{$problem['title']}</h2>";
 		
-		$shown = array();
-		$highest = 0;
+		foreach ($problem['events'] as $event) {
+			if($event['type']=='assign') { // the event is an assignment (formatted differently)
+				echo "<div class='assign'>
+				<h3>{$event['by']['name']} <em>#{$event['by']['id']}</em> assigned the problem to {$event['to']['name']} <i>#{$event['to']['id']}</i></h3>
+				<p>";
 
-		do {
-			$high = 0;
-			foreach ($problem['calls'] as $key => $call) {
-				$callDate = strtotime($call['date']);
-				if($callDate > $high)
-					$high = $callDate;
-				if($callDate == $highest) {
-					echo outputResponse($call,'call',$format);
-					unset($problem['calls'][$key]);
-					continue;
-				}
+				echo date($format,$event['date']);
+
+				if($ago = longAgoEpoch($event['date']))
+					echo '<br><em>('.$ago.')</em>';
+
+				echo "</p></div>";
 			}
+			else { // a message/call/solution
+				echo "<div class='response'>
+				<div class='staff'>
+				<p><strong>";
 
-			foreach ($problem['messages'] as $key => $mess) {
-				$messDate = strtotime($mess['date']);
-				if($messDate > $high)
-					$high = $messDate;
-				if($messDate == $highest) {
-					echo outputResponse($mess,'message',$format);
-					unset($problem['messages'][$key]);
-					continue;
+				switch($event['type']) {
+					case 'call':
+						echo "Call with {$event['caller']['name']} <br>on {$event['caller']['tel']}";
+						break;
+					case 'message':
+						echo 'Message';
+						break;
+					case 'solved':
+						echo 'Solution';
+						break;
 				}
+
+				echo "</strong>
+				<br>" . date($format,$event['date']);
+
+				if($ago = longAgoEpoch($event['date']))
+				echo "<br><em>($ago)</em>";
+
+				echo "</p>
+
+				<p>{$event['specialist']['name']} <em>{$event['specialist']['id']}</em><br>
+				{$event['specialist']['tel']}<br>
+				<em>{$event['specialist']['job']}</em></p>
+				</div>
+
+				<div class='notes'>
+				<h3>".($event['type']=='solution' ? 'Solution' : $event['subject'])."</h3>
+				<p>{$event['message']}</p>
+				</div>
+				</div>";
 			}
-
-			foreach ($problem['assign'] as $key => $ass) {
-				$assDate = strtotime($ass['date']);
-				if($assDate > $high)
-					$high = $assDate;
-				if($assDate == $highest) {
-					echo "<div class='assign'>
-					<h3>{$tlogin[$ass['by']]['name']} assigned the problem to {$tlogin[$ass['op']]['name']}</h3>
-					<p>";
-
-					echo date($format,$assDate);
-		
-
-					if($ago = longAgo($ass['date']))
-						echo '<br><em>('.$ago.')</em>';
-
-					echo "</p></div>";
-
-					unset($problem['assign'][$key]);
-					continue;
-				}
-			}
-
-			if($solved) {
-				$solDate = strtotime($problem['solution']['date']);
-				if($solDate > $high)
-					$high = $solDate;
-				if($solDate == $highest) {
-					echo outputResponse($problem['solution'],'solution',$format);
-					unset($problem['solution']);
-					continue;
-				}
-			}
-
-			$highest = $high;
-		} while ((count($problem['calls'])!=0) || (count($problem['messages'])!=0) || (count($problem['assign'])!=0) || (count($problem['solution'])!=0));
-
-
-		echo "</td>
-		</tr>";
-	}
-
-
-	function outputResponse($response, $type, $format) {
-		global $tlogin;
-
-		$op = $tlogin[$response['op']];
-
-		$ret = "<div class='response'>
-		<div class='staff'>
-		<p><strong>";
-		
-		switch($type) {
-			case 'call':
-				$ret .= 'Call with '.$response['caller'];
-				break;
-			case 'message':
-				$ret .= 'Message';
-				break;
-			case 'solution':
-				$ret .= 'Solution';
-				break;
 		}
 
-		$ret .= "</strong>
-		<br>" . date($format,strtotime($response['date'].' '.date_default_timezone_get()));
-
-		if($ago = longAgo($response['date']))
-			$ret .= '<br><em>('.$ago.')</em>';
-
-		$ret .= "</p>
-
-		<p>{$op['name']}<br>
-		<em>{$op['job']}</em></p>
-		</div>
-
-		<div class='notes'>
-		<h3>".($type=='solution' ? 'Solution' : $response['title'])."</h3>
-		<p>{$response['text']}</p>
-		</div>
-		</div>";
-
-		return $ret;
+		echo "</td></tr>";
 	}
 ?>
--- pages/problems.php
+++ pages/problems.php
@@ -5,14 +5,20 @@ Outputs a list of unsolved problems assigned to that user, or all of them if the
 Change log
 ==========
 
-14/2/17 - Gareth Nunns
-Added changelog
+18/2/17 - Gareth Nunns
+Used the provided SQL to output table
+
+
 14/2/17 - Lewys Bonds
 Added SQL and made it user specific
 
+14/2/17 - Gareth Nunns
+Added changelog
+
 */
 
-	require_once dirname(__FILE__).'/../check.php';
+	require_once dirname(__FILE__).'/../check.php'; // check the user is logged in
+	require_once dirname(__FILE__).'/../site/secure.php'; // connect to the database
 ?>
 
 <h1>Problems</h1>
@@ -23,24 +29,76 @@ Added SQL and made it user specific
 		<th>Title</th>
 		<th>Customers</th>
 		<th>Operators</th>
-		<th colspan="3">Calls</th>
+		<th colspan="3">Activity</th>
 	</tr>
 
 <?php
-$stmt = $conn->prepare(SELECT * FROM problem LEFT JOIN message ON problem.idProblem = message.idProblem LEFT JOIN emp ON message.specialist = emp.idEmp LEFT JOIN jobTitle ON emp.jobTitle =jobTitle.idJobTitle LEFT JOIN assign ON problem.idProblem = assign.idProblem LEFT JOIN calls ON problem.idProblem = calls.idProblem LEFT JOIN type ON problem.idType =type.idType WHERE emp.idEmp = :empid GROUP BY problem.idProblem, message.date);
+
+	$sql = "
+SELECT problem.idProblem, problem.title, type.idType, type.name AS type,
+
+-- messages
+message.idMessage, message.date AS messDate, message.subject AS messSub, message.message AS messMess, message.specialist AS messSpec,
+CONCAT(memp.firstName, ' ', memp.surname) AS messName, memp.tel AS messTel, mempJob.name AS messJob,
+
+-- assignments
+assign.idAssign, assign.assBy, CONCAT(assByemp.firstName, ' ', assByemp.surname) AS assByName,
+assign.assTo, CONCAT(assToemp.firstName, ' ', assToemp.surname) AS assToName, assDate,
+
+-- calls
+calls.idCalls, calls.caller, CONCAT(calleremp.firstName, ' ', calleremp.surname) AS callerName, calleremp.tel AS callerTel, callerJob.name AS callerJob,
+calls.op, CONCAT(opemp.firstName, ' ', opemp.surname) AS opName, opemp.tel AS opTel, opJob.name AS opJob,
+calls.date AS callDate, calls.subject AS callSubject, calls.notes AS callNotes
+
+FROM problem
+
+-- JOINS
+-- type
+LEFT JOIN type ON problem.idType = type.idType 
+
+-- messages
+LEFT JOIN message ON problem.idProblem = message.idProblem
+LEFT JOIN emp AS memp ON message.specialist = memp.idEmp
+LEFT JOIN jobTitle as mempJob ON memp.jobTitle = mempJob.idJobTitle
+
+-- assignments
+LEFT JOIN assign ON problem.idProblem = assign.idProblem
+LEFT JOIN emp AS assByemp ON assign.assBy = assByemp.idEmp
+LEFT JOIN emp AS assToemp ON assign.assTo = assToemp.idEmp
+
+-- calls
+LEFT JOIN calls ON problem.idProblem = calls.idProblem
+LEFT JOIN emp AS calleremp ON calls.caller = calleremp.idEmp
+LEFT JOIN jobTitle as callerJob ON calleremp.jobTitle = callerJob.idJobTitle
+LEFT JOIN emp AS opemp ON calls.op = opemp.idEmp
+LEFT JOIN jobTitle as opJob ON opemp.jobTitle = opJob.idJobTitle
+
+ORDER BY idProblem, message.date DESC, assDate DESC, calls.date DESC
+
+-- temporarily removed
+-- WHERE emp.idEmp = :empid";
+
+/*
 if ($_SESSION['sudo']){
 	$stmt->bindParam(':empid', *);
 } else{
 	$stmt->bindParam(':empid', $_SESSION[empid]);	
 }
-	/*foreach ($tproblems as $problem)
-		if(!$problem['solution']) // it hasn't been solved
-			outputProblem($problem,false);*/
+*/
+
+	$sth = $dbh->prepare($sql);
+
+	$sth->execute();
+
+	$problems = storeProblems($sth->fetchAll());
+
+	foreach ($problems as $id => $problem)
+		outputProblem($id,$problem);
 ?>
 </table>
 
 <script type="text/javascript">
-	/*$('tr:nth-of-type(3n+4)').hide().each(function() {
+	$('tr:nth-of-type(3n+4)').hide().each(function() {
 		$(this).children().first().children().slideUp();
 	});
 
@@ -53,5 +111,5 @@ if ($_SESSION['sudo']){
 		$('html, body').delay(300).animate({
 			scrollTop: $(window.location.hash).offset().top-80
 		}, 400);
-	}*\
+	}
 </script>
