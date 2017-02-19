83f658a63798731f94b132a2aacc35cdef5befa8
Gareth Nunns <garethnunns@gmail.com>
2017-02-19 00:14:41 +0000

    Search problems
    COGS-12 #time 20m Can now search through existing problems

--- ajax/problems.php
+++ ajax/problems.php
@@ -5,25 +5,40 @@ Search results for problems
 Change log
 ==========
 
+18/2/17 - Gareth Nunns
+Linked to database
+
 14/2/17 - Gareth Nunns
 Added changelog
 
 */
+	require_once dirname(__FILE__).'/../check.php'; // check the user is logged in
+	require_once dirname(__FILE__).'/../site/secure.php'; // connect to the database
 
-	require_once dirname(__FILE__).'/../check.php';
+	$sql = "SELECT problem.idProblem, problem.title, COUNT(calls.idCalls) AS 'calls', COUNT(solved.idProblem) AS 'solved'
+			FROM problem
+			LEFT JOIN calls ON problem.idProblem = calls.idProblem
+			LEFT JOIN solved ON problem.idProblem = solved.idProblem
+			WHERE problem.title LIKE :psp
+			OR problem.idProblem LIKE :sp
+			GROUP BY problem.idProblem
+			ORDER BY problem.idProblem
+			LIMIT 10";
 
-	if(!empty($_GET['s'])) {
-		$problems = [];
+	$sth = $dbh->prepare($sql);
 
-		foreach ($tproblems as $problem)
-			if(stripos($problem['title'], $_GET['s']))
-				$problems[$problem['id']] = $problem['title'].($problem['solution'] ? ' - Solved' : '');
+	// sanitize inputs
+	$sp = $_GET['s'].'%';
+	$psp = '%'.$sp;
 
-		krsort($problems);
+	$sth->bindParam(':sp', $sp);
+	$sth->bindParam(':psp', $psp);
 
-		foreach ($problems as $id => $prob)
-			echo "<p data-id='$id'><strong>$id</strong> - $prob</p>";
-		
-		echo "<p>Create a new problem with the title <strong>'{$_GET['s']}'</strong></p>";
-	}
+	$sth->execute();
+
+	// output the results (if there were any)
+	echo "<p>Create a new problem with the title <strong>'{$_GET['s']}'</strong></p>";
+	if($sth->rowCount())
+		foreach ($sth->fetchAll() as $row) 
+			echo "<p data-id='{$row['idProblem']}'><strong>{$row['idProblem']}</strong> - {$row['title']} ({$row['calls']} ".translate($row['calls'] == 1 ? 'call' : 'calls').") - ".translate($row['solved'] ? 'Solved' : 'Unsolved')."</p>";
 ?>
