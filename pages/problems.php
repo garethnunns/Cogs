ce15bf7a017cc67a3b353982fc3c9a82ff573c7e
Gareth Nunns <garethnunns@gmail.com>
2017-02-20 01:46:10 +0000

    Other problems
    COGS-14 #time 25m Added the other problems - probably correct.

--- pages/problems.php
+++ pages/problems.php
@@ -5,6 +5,9 @@ Outputs a list of unsolved problems assigned to that user, or all of them if the
 Change log
 ==========
 
+20/2/17 - Gareth Nunns
+Added the problems that the user hasn't been assigned too
+
 18/2/17 - Gareth Nunns
 Used the provided SQL to output table
 
@@ -22,6 +25,8 @@ Added changelog
 ?>
 
 <h1>Problems</h1>
+
+<h2>Your problems</h2>
 
 <?php
 
@@ -97,6 +102,84 @@ WHERE problem.idProblem NOT IN (SELECT solved.idProblem FROM solved) ";
 	}
 	else
 		echo '<p>'.translate('There are currently no problems assigned to you :)').'</p>';
+?>
+
+<h2>Other problems</h2>
+
+<?php
+
+	session_start();
+
+	$sql = "
+SELECT problem.idProblem, problem.title, type.idType, type.name AS type,
+
+-- messages
+message.idMessage, message.date AS messDate, message.subject AS messSub, message.message AS messMess, message.specialist AS messSpec,
+CONCAT(memp.firstName, ' ', memp.surname) AS messName, memp.tel AS messTel, mempJob.name AS messJob,
+
+-- assignments
+assign.idAssign, assign.assBy, CONCAT(assByemp.firstName, ' ', assByemp.surname) AS assByName,
+assign.assTo, CONCAT(assToemp.firstName, ' ', assToemp.surname) AS assToName, assDate,
+
+-- calls
+calls.idCalls, calls.caller, CONCAT(calleremp.firstName, ' ', calleremp.surname) AS callerName, calleremp.tel AS callerTel, callerJob.name AS callerJob,
+calls.op, CONCAT(opemp.firstName, ' ', opemp.surname) AS opName, opemp.tel AS opTel, opJob.name AS opJob,
+calls.date AS callDate, calls.subject AS callSubject, calls.notes AS callNotes
+
+FROM problem
+
+-- JOINS
+-- type
+LEFT JOIN type ON problem.idType = type.idType 
+
+-- messages
+LEFT JOIN message ON problem.idProblem = message.idProblem
+LEFT JOIN emp AS memp ON message.specialist = memp.idEmp
+LEFT JOIN jobTitle as mempJob ON memp.jobTitle = mempJob.idJobTitle
+
+-- assignments
+LEFT JOIN assign ON problem.idProblem = assign.idProblem
+LEFT JOIN emp AS assByemp ON assign.assBy = assByemp.idEmp
+LEFT JOIN emp AS assToemp ON assign.assTo = assToemp.idEmp
+
+-- calls
+LEFT JOIN calls ON problem.idProblem = calls.idProblem
+LEFT JOIN emp AS calleremp ON calls.caller = calleremp.idEmp
+LEFT JOIN jobTitle as callerJob ON calleremp.jobTitle = callerJob.idJobTitle
+LEFT JOIN emp AS opemp ON calls.op = opemp.idEmp
+LEFT JOIN jobTitle as opJob ON opemp.jobTitle = opJob.idJobTitle
+
+WHERE problem.idProblem NOT IN (SELECT solved.idProblem FROM solved) ";
+
+	if(!$_SESSION['sudo'])
+		$sql .= " AND assign.assTo <> '{$_SESSION['user']}' AND (SELECT MAX(assDate) FROM assign AS a1 WHERE a1.idProblem <> assign.idProblem) ";
+
+	$sql .= "ORDER BY idProblem, message.date DESC, assDate DESC, calls.date DESC";
+
+	$sth = $dbh->prepare($sql);
+
+	$sth->execute();
+
+	if($sth->rowCount()) {
+?>
+	<table class="problems">
+		<tr>
+			<th>ID</th>
+			<th>Title</th>
+			<th>Customers</th>
+			<th>Operators</th>
+			<th colspan="3">Activity</th>
+		</tr>
+<?php
+		$problems = storeProblems($sth->fetchAll());
+
+		foreach ($problems as $id => $problem)
+			outputProblem($id,$problem);
+
+		echo '</table>';
+	}
+	else
+		echo '<p>'.translate('There are currently no other problems').'</p>';
 ?>
 
 <script type="text/javascript">
